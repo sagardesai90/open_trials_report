50d0480e032cd99819a4177260af5ddaa4333942
sagardesai90 <saagardesai@gmail.com>
2018-11-19 14:22:51 -0600

    cleaning up folders and adding axios

--- open-trials-report/src/server.js
+++ open-trials-report/src/server.js
@@ -3,3 +3,22 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const server = express();
 
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get('/', function(req, res){
+    res.status(200).json({api: 'running'});
+});
+
+const port = process.env.PORT || 9003;
+mongoose.promise = global.Promise; 
+
+mongoose.connect('mongodb://localhost/search', {}, err => {
+    if (err) console.log(err);
+    console.log("Mongoose is connected to your database.")
+})
+
+server.listen(port, () => {
+    console.log(`\n===API is running on http://localhost:${port} ===\n`);
+});
